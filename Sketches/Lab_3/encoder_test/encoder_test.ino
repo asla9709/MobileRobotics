259490d7b608e035ad1b04040cb92f61e9e815d9
Caspian Peavyhouse <caspian.peavyhouse@gmail.com>
2018-04-23 16:37:31 -0400

    Updated Encoder Test
    Updated the encoder test

--- Sketches/Lab_3/encoder_test/encoder_test.ino
+++ Sketches/Lab_3/encoder_test/encoder_test.ino
@@ -9,16 +9,15 @@ Motor leftMotor(2,3);
 Motor rightMotor(4,5);
 Car myCar(&leftMotor,&rightMotor);
 
-I2CEncoder encoder;
-I2CEncoder encoder2;
-I2CEncoder encoder3;
+I2CEncoder leftEncoder;
+I2CEncoder rightEncoder;
 
 void setup() {
   // put your setup code here, to run once: 
   Wire.begin();
-  encoder.init(MOTOR_393_SPEED_ROTATIONS,MOTOR_393_TIME_DELTA);
-  encoder.unTerminate();
-  encoder2.init(MOTOR_393_SPEED_ROTATIONS,MOTOR_393_TIME_DELTA);
+  leftEncoder.init(MOTOR_393_SPEED_ROTATIONS,MOTOR_393_TIME_DELTA);
+  leftEncoder.unTerminate();
+  rightEncoder.init(MOTOR_393_SPEED_ROTATIONS,MOTOR_393_TIME_DELTA);
   
 
   Serial.begin(9600);
@@ -26,5 +25,24 @@ void setup() {
 }
 
 void loop() {
+  leftMotor.forward(50);
+  rightMotor.forward(50);
+  
+  int spdL = leftEncoder.getSpeed();
+  int posL = leftEncoder.getRawPosition();
 
+  int spdR = rightEncoder.getSpeed();
+  int posR = rightEncoder.getRawPosition();
+
+  Serial.print("Left Motor Speed:\t");
+  Serial.println(spdL);
+  Serial.print("Left Motor Position:\t");
+  Serial.println(posL);
+  Serial.println("");
+
+  Serial.print("Right Motor Speed:\t");
+  Serial.println(spdR);
+  Serial.print("Right Motor Position:\t");
+  Serial.println(posL);
+  Serial.println("");
 }
