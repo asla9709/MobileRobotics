2fd1a4f54d926ac77d6040f109772deca9c7b94f
asla9709 <komodo009@gmail.com>
2018-06-13 17:16:15 -0400

    FINISHED CAMERA CRAP

--- Robot/Camera/Robot.py
+++ Robot/Camera/Robot.py
@@ -5,23 +5,25 @@ import math
 import serial
 
 
+font = cv2.FONT_HERSHEY_SIMPLEX
+
 class Robot:
     def __init__(self):
         self.initialize_camera()
-        self.initialize_serial()
+        # self.initialize_serial()
         
     
     def initialize_camera(self):
         self.width = 640
         self.height = 480
         self.Xcenter = self.width / 2
-        self.video = cv2.VideoCapture(0)
+        self.video = cv2.VideoCapture(1)
         self.video.set(cv2.CAP_PROP_FRAME_WIDTH, self.width)
         self.video.set(cv2.CAP_PROP_FRAME_HEIGHT, self.height)
         
         self.horizon = 300#int(self.height / 2)
-        self.robot_left_side = 50
-        self.robot_right_side = 450
+        self.robot_left_side = 25
+        self.robot_right_side = 475
         self.roi_height = self.height - self.horizon
         self.robot_center = int((50 + 450) / 2)
         self.initialize_edge_settings()
@@ -39,7 +41,7 @@ class Robot:
         cv2.createTrackbar('reps','settings', 1, 100, self.nothing)
         cv2.createTrackbar('aeps','settings', 1, 100, self.nothing)
         cv2.createTrackbar('threshold1','settings', 0, 300, self.nothing)
-        cv2.createTrackbar('threshold2','settings', 400, 1000, self.nothing)
+        cv2.createTrackbar('threshold2','settings', 350, 1000, self.nothing)
         
     
     def lane_detection(self, follow = False):
@@ -84,111 +86,82 @@ class Robot:
             '''
             count = 0
             
-            #if  len(contour) == 2:
-            left_lane, right_lane = self.extract_lines(contour)     
-            ''''
-            if left_lane is not None and right_lane is not None:
-                cv2.drawContours(image,[left_lane],0, (255, 0, 0),-1)
-                left_average = np.mean([left_lane], 1)[0][0]
-                cv2.circle(image, (int(left_average[0]), int(left_average[1])), 5, (0, 0, 255))
-                cv2.drawContours(image,[right_lane],0, (0, 255, 0),-1)
-                right_average = np.mean([right_lane], 1)[0][0]
-                cv2.circle(image, (int(right_average[0]), int(right_average[1])), 5, (0, 0, 255))
-                
-                x_avg = int((left_average[0] + right_average[0])/ 2)
-                y_avg = int((left_average[1] + right_average[1])/ 2)
-                cv2.circle(image, (x_avg, y_avg), 10, (255, 255, 0))
-                
-                if follow:
-                    dataLine = "X{0}R{1}\n".format(x_avg, 14)
-                    self.ser.write(dataLine.encode('utf-8'))
-            '''
-            right_correction = 0
-            left_correction = 0
-            if left_lane is not None:
-                left_point = tuple(left_lane[left_lane[:, :, 1].argmax()][0])
-                cv2.drawContours(image,[left_lane],0, (255, 0, 0),-1)
-                cv2.circle(image, (left_point[0],left_point[1]), 5, (0, 0, 255))
-                left_correction = int(left_point[0] - (self.robot_left_side- 25))
-                cv2.circle(image, (self.robot_center + left_correction, int(left_point[1])), 10, (0, 255, 255))
-                #if follow:
-                    #dataLine = "X{0}V{1}\n".format(self.robot_center + left_correction, 17)
-                    #self.ser.write(dataLine.encode('utf-8'))
-            if right_lane is not None:
-                right_point = tuple(right_lane[right_lane[:, :, 1].argmax()][0])
-                cv2.drawContours(image,[right_lane],0, (0, 255, 0),-1)
-                cv2.circle(image, (right_point[0],right_point[1]), 5, (0, 0, 255))
-                right_correction = int(right_point[0] - (self.robot_right_side + 25))
-                cv2.circle(image, (self.robot_center + right_correction, int(right_point[1])), 10, (0, 255, 255))
-                
-                #if follow:
-                    #dataLine = "X{0}R{1}\n".format(self.robot_center + correction, 17)
-                    #self.ser.write(dataLine.encode('utf-8'))
-            if left_lane is not None or right_lane is not None:
-                if follow:
-                    cv2.circle(image, (self.robot_center + left_correction + right_correction, self.roi_height), 5, (0, 0, 255))
-                    dataLine = "X{0}R{1}\n".format(self.robot_center + left_correction + right_correction, 15)
-                    self.ser.write(dataLine.encode('utf-8'))
+            left_lane, right_lane = self.extract_lines(contour)
+
+            if not left_lane is None and  not right_lane is None:
+                x,y,w,h = cv2.boundingRect(left_lane)
+                mid_left = math.floor(x + w/2)
+                x, y, w, h = cv2.boundingRect(right_lane)
+                mid_right = math.floor(x + w / 2)
+
+                mid = math.floor((mid_left+mid_right)/2)
+
+                cv2.circle(image, (math.floor(mid), 30), 5, (255,255,255))
+                cv2.line(image,(mid_left,0),(mid_left,200),(255,255,255))
+                cv2.line(image,(mid_right,0),(mid_right,200),(255,255,255))
+
+                xoffset = (mid - self.robot_center)
+                # self.suggested_move = (xoffset, 50)
+
+            elif not left_lane is None:
+                x,y,w,h = cv2.boundingRect(left_lane)
+                mid_left = math.floor(x + w/2)
+
+                xoffset = mid_left - self.robot_left_side
+
+            elif not right_lane is None:
+                x,y,w,h = cv2.boundingRect(right_lane)
+                mid_right = math.floor(x + w/2)
+
+                xoffset = mid_right - self.robot_right_side
+
             else:
-                if follow:
-                    dataLine = "X{0}R{1}\n".format(self.robot_center, -1)
-                    self.ser.write(dataLine.encode('utf-8'))
-                '''vx,vy, x0, y0 = cv2.fitLine(contour[0], cv2.DIST_L12, 0, reps, aeps)
-                m=600
-                cv2.line(image, (x0-m*vx[0], y0-m*vy[0]), (x0+m*vx[0], y0+m*vy[0]), (255,0,255))
-                average_point = np.mean([contour[0]], 1)[0][0]
-                #print(average_point)
-                cv2.circle(image, (int(average_point[0]), int(average_point[1])), 5, (255,0, 255))
-                
-                
-                vx,vy, x0, y0 = cv2.fitLine(contour[1], cv2.DIST_L12, 0, reps, aeps)
-                m=600
-                cv2.line(image, (x0-m*vx[0], y0-m*vy[0]), (x0+m*vx[0], y0+m*vy[0]), (0,255,255))
-                average_point = np.mean([contour[1]], 1)[0][0]
-                #print(average_point)
-                cv2.circle(image, (int(average_point[0]), int(average_point[1])), 5, (0, 255, 255))
-                #print(leftLine)
-                count += 1
-                
-                #print(cnt)
-                '''
+                xoffset = 0
+
+            cv2.line(image, (self.robot_center,150), (self.robot_center + xoffset,150), (255,255,0))
+
+
+
+
+            # right_correction = 0
+            # left_correction = 0
+            #
+            # if left_lane is not None:
+            #     cv2.drawContours(image,[left_lane],0, (255, 0, 0),-1)
+            #     left_point = tuple(left_lane[left_lane[:, :, 1].argmax()][0])
+            #     cv2.circle(image, (left_point[0],left_point[1]), 5, (0, 0, 255))
+            #
+            #     left_correction = int(left_point[0] - (self.robot_left_side- 25))
+            #     cv2.circle(image, (self.robot_center + left_correction, int(left_point[1])), 10, (0, 255, 255))
+            #     #if follow:
+            #         #dataLine = "X{0}V{1}\n".format(self.robot_center + left_correction, 17)
+            #         #self.ser.write(dataLine.encode('utf-8'))
+            # if right_lane is not None:
+            #     cv2.drawContours(image,[right_lane],0, (0, 255, 0),-1)
+            #     right_point = tuple(right_lane[right_lane[:, :, 1].argmax()][0])
+            #     cv2.circle(image, (right_point[0],right_point[1]), 5, (0, 0, 255))
+            #
+            #     right_correction = int(right_point[0] - (self.robot_right_side + 25))
+            #     cv2.circle(image, (self.robot_center + right_correction, int(right_point[1])), 10, (0, 255, 255))
+            #
+            #     #if follow:
+            #         #dataLine = "X{0}R{1}\n".format(self.robot_center + correction, 17)
+            #         #self.ser.write(dataLine.encode('utf-8'))
+            # if left_lane is not None or right_lane is not None:
+            #     if follow:
+            #         cv2.circle(image, (self.robot_center + left_correction + right_correction, self.roi_height), 5, (255, 0, 0))
+            #         dataLine = "X{0}R{1}\n".format(self.robot_center - left_correction + right_correction, 15)
+            #         # self.ser.write(dataLine.encode('utf-8'))
+            # else:
+            #     if follow:
+            #         dataLine = "X{0}R{1}\n".format(self.robot_center, -1)
+            #         # self.ser.write(dataLine.encode('utf-8'))
             
             cv2.line(image,(self.robot_center, 0),(self.robot_center, self.roi_height),(0,0,255),2)
             cv2.line(image,(self.robot_left_side, 0),(self.robot_left_side, self.roi_height),(0,0,255),2)
             cv2.line(image,(self.robot_right_side, 0),(self.robot_right_side, self.roi_height),(0,0,255),2)
             
-            #opening = cv2.morphologyEx(edged, cv2.MORPH_OPEN, k)
-            #one = cv2.morphologyEx(edged, cv2.MORPH_CLOSE, ks, iterations = dIterations)
-            #two = cv2.morphologyEx(edged, cv2.MORPH_CLOSE, ke, iterations = dIterations)
-            '''
-            hough_lines = cv2.HoughLinesP(two,2,np.pi/180,10,minLineLength,maxLineGap)
-            cv2.line(blank_image,(0,self.horizon),(self.width,self.horizon),(0,0,255),2)
-            #lines = list()
-            lines, a,b,c = line_detector.detect(two)
-            line_detector.drawSegments(blank_image, lines)
 
-            if hough_lines is not None:
-            #print(b)
-                right_lines, left_lines = self.separate_lines(hough_lines)
-                for x in range(0, len(left_lines)):
-                   for x1,y1,x2,y2 in left_lines[x]:
-                       if(y2 < self.horizon):
-                           #continue
-                           pass
-                       cv2.line(blank_image,(x1,y1),(x2,y2),(0,255,0),2)
-                       
-                for x in range(0, len(right_lines)):
-                   for x1,y1,x2,y2 in right_lines[x]:
-                       if(y2 < self.horizon):
-                           pass#continue
-                       cv2.line(blank_image,(x1,y1),(x2,y2),(255,0,255),2)
-                                            
-            #'''    
-                #cv2.polylines(blank_image,  [points],  False,  (0, 255, 255))
-            #line_detector.compareSegments((self.width, self.height), hough_lines, lines_2, blank_image)
-            
-            #blank_image, x_heading = self.draw_midpoint((self.width, self.height), blank_image, lines)
-            #cv2.imshow("test",edged)
             cv2.imshow("Frame_2", out)
             cv2.imshow("Frame_3", image)
             key = cv2.waitKey(1) & 0xFF
@@ -217,13 +190,18 @@ class Robot:
         #print(np.minimum([contours[0]]))
         if count == 0:
             return None, None
+
         if count == 1:
-            average_x = np.mean([contours[0]], 1)[0][0][0]
-            
-            if average_x > self.robot_center:
+            rect = cv2.minAreaRect(contours[0])
+            angle = rect[2]
+            if (rect[1][0] > rect[1][1]):
+                angle = angle + 90
+
+            if angle < 0:
                 return None, contours[0]
             else:
                 return contours[0], None
+
         if count == 2:
             first_average_x = np.mean([contours[0]], 1)[0][0][0]
             second_average_x = np.mean([contours[1]], 1)[0][0][0]
@@ -232,25 +210,18 @@ class Robot:
                 return contours[1], contours[0]
             else:
                 return contours[0], contours[1]
+
         if count > 2:
-            avg_point_list = list()
-            
-            for i in range(0, count):
-                avg_point_list.append(np.mean([contours[i]], 1)[0][0])
-                
-            left = avg_point_list[0]
-            right = avg_point_list[0]
-            return_list = list()
-            for point in avg_point_list:
-                if abs(point[0] - self.robot_left_side) < abs(left[0] - self.robot_left_side):
-                    left = point
-                if abs(point[0] - self.robot_right_side) < abs(right[0] - self.robot_right_side):
-                    right = point
-            
-            for i in range(0, count):
-                if (avg_point_list[i][0] == left[0] and avg_point_list[i][1] == left[1]) or (avg_point_list[i][0] == right[0] and avg_point_list[i][0] == right[0]):
-                    return_list.append(contours[i])
-            return self.extract_lines(return_list) #recursive                
+            left, right = contours[0], contours[0]
+            for contour in contours:
+                lowest_x = tuple(contour[contour[:, :, 1].argmax()][0])
+                left_lowest_x = tuple(left[left[:, :, 1].argmax()][0])
+                right_lowest_x = tuple(right[right[:, :, 1].argmax()][0])
+                if abs(lowest_x[0] - self.robot_left_side) < abs(left_lowest_x[0] - self.robot_left_side):
+                    left = contour
+                elif abs(lowest_x[0] - self.robot_right_side) < abs(right_lowest_x[0] - self.robot_right_side):
+                    right = contour
+            return left, right
     
     @staticmethod
     def nothing(x):
@@ -258,3 +229,7 @@ class Robot:
 
         
         
+if __name__ == '__main__':
+    robot = Robot()
+    lane_follow = True
+    robot.lane_detection(lane_follow)
